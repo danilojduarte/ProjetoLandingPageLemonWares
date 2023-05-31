80339a110aede6c9957a1913229776f0105f4275
DaniloDuarte <danilojoseduarte@gmail.com>
2023-05-31 09:54:31 -0300

    Pausa na aula em {01-17-57} Criando Menu List

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 function onScroll() {
-console.log(scrollY > 0)
-
-
-//   if (scrollY > 0){
-//   navegacao.classList.add('scroll')
-// } else {
-//   navegacao.classList.remove('scroll')
-// }
-
+  if (scrollY > 0) {
+    navegacao.classList.add("scroll");
+  } else {
+    navegacao.classList.remove("scroll");
+  }
 }
